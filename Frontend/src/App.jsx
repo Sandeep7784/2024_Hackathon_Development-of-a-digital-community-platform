03373f0aa3df9923fc7368a5ca401799ad934d19
Sandeep <singh.192@iitj.ac.in>
2024-02-29 18:46:54 +0530

    addded the routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,18 +5,24 @@ import Header from "./components/Header";
 import User from "./components/User";
 import QuestHistory from "./components/QuestHistory";
 import PendingRequests from "./components/PendingRequests";
+import CommunityUser from "./components/CommunityUser";
+import AddQuest from "./components/AddQuest";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <React.Fragment>
-      {/* <Header /> */}
-      {/* <SignIn /> */}
-      <SignUp />
-      {/* <User /> */}
-      {/* <QuestHistory /> */}
-      {/* <PendingRequests /> */}
-    </React.Fragment>
+    <Router>
+      <Routes>
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/user" element={<User />} />
+        <Route path="/questHistory" element={<QuestHistory />} />
+        <Route path="/community-manager" element={<CommunityUser />} />
+        <Route path="/addQuest" element={<AddQuest />} />
+        <Route path="/pendingRequests" element={<PendingRequests />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ Frontend/src/components/CommunityUser.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import Header from "./Header";
+import Stack from "@mui/material/Stack";
+import { Button } from "@mui/material";
+import AssignmentTurnedInIcon from "@mui/icons-material/AssignmentTurnedIn";
+import ContactSupportIcon from "@mui/icons-material/ContactSupport";
+import Paper from "@mui/material/Paper";
+import InputBase from "@mui/material/InputBase";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
+import CreateIcon from "@mui/icons-material/Create";
+
+export default function User() {
+  return (
+    <React.Fragment>
+      <Header />
+      <section className="flex justify-center items-center h-screen mb-12 md:mb-48 py-16 md:py-24">
+        <div className="container mx-auto text-center">
+          <h2 className="font-bold leading-tight text-gray-900 text-4xl md:text-5xl lg:text-6xl mb-4 md:mb-8">
+            Hello Mr. XYZ Singh
+            {/* <span className="text-gradient"> SaaS Founders</span> */}
+          </h2>
+
+          <div className="max-w-3xl mx-auto">
+            <p className="text-gray-800 text-lg md:text-xl mt-4 md:mt-6 mb-8">
+              Comprehensive Solutions for Aspiring Opportunity Seekers
+            </p>
+          </div>
+          <div
+            className="flex items-center justify-center md:justify-start mt-6"
+            style={{
+              marginTop: "1.5rem",
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center", // Center horizontally
+              alignItems: "center", // Center vertically
+            }}
+          >
+            <Stack
+              direction="row"
+              spacing={2}
+              alignItems="center"
+              justifyContent="center"
+            >
+              <Button
+                variant="contained"
+                size="large"
+                startIcon={<AssignmentTurnedInIcon />}
+                disableElevation
+              >
+                Quest's History
+              </Button>
+              <Button
+                variant="contained"
+                size="large"
+                startIcon={<ContactSupportIcon />}
+                disableElevation
+              >
+                Pending Request's
+              </Button>
+              <Button
+                startIcon={<CreateIcon />}
+                size="large"
+                variant="contained"
+                color="primary"
+              >
+                Add Quest
+              </Button>
+            </Stack>
+          </div>
+        </div>
+      </section>
+    </React.Fragment>
+  );
+}
